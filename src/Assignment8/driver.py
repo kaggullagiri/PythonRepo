388ebbec3cc2d8a268506522e4ffbd03ccea629c
kaggullagiri <150141024+kaggullagiri@users.noreply.github.com>
2023-11-23 16:30:32 +0530

    Printing the pattern problem executed succesfully

--- src/Assignment8/driver.py
+++ src/Assignment8/driver.py
@@ -1,7 +1,7 @@
 from src.Assignment8.util import printingpattern
 
 # Taking the user input for the thickness
-thickness = int(input("Enter thickness (must be an odd number): "))
+thickness = int(input("Enter thickness (Enter an odd number): "))
 
 # Calling the utility function to generate and print the pattern
 result = printingpattern(thickness)
