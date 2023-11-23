892e446600d4c399abe877bf4da68cce92311d6e
kaggullagiri <150141024+kaggullagiri@users.noreply.github.com>
2023-11-23 16:32:52 +0530

    Merge tools problem executed

--- src/Assignment7/driver.py
+++ src/Assignment7/driver.py
@@ -1,7 +1,8 @@
-from util import split_num_char
+from src.Assignment7.util import split_num_char
 
 if __name__ == '__main__':
     string, k = input("Enter the String Value= "), int(input("Enter the Key Value= "))
     result = list(split_num_char(string, k))
     for word in result:
         print(word)
+        
