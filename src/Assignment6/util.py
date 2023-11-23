60af912a430cb586c71b5d36005bc7a9e9997628
kaggullagiri <150141024+kaggullagiri@users.noreply.github.com>
2023-11-23 16:28:29 +0530

    String formatting problem executed successfully

--- src/Assignment6/util.py
+++ src/Assignment6/util.py
@@ -3,3 +3,4 @@ def print_formatted(number):
     for i in range(1, number + 1):
         deci, octa, hexa, bina = str(i), oct(i)[2:], hex(i)[2:].upper(), bin(i)[2:]
         print(deci.rjust(width), octa.rjust(width), hexa.rjust(width), bina.rjust(width))
+        
