7254b9e69c6d7b679ec96bfbc69e58f0e5ea7e38
kaggullagiri <150141024+kaggullagiri@users.noreply.github.com>
2023-11-24 14:29:45 +0530

    This is a util file for Finding Average Marks Problem

--- src/Assignment9/util.py
+++ src/Assignment9/util.py
@@ -4,3 +4,4 @@ def average_marks(num_students, fields, student_data):
     students = namedtuple('my_student', fields)
     total_marks = sum(int(students(*student_data()).MARKS) for _ in range(num_students))
     return total_marks / num_students
+    
